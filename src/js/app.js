e8e7da3443f82f2918f063ba920943ffe946f232
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-06-23 19:46:46 +0000

    entrega conditional profile

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name ? variables.name : "Nombre"} ${
+    variables.lastname ? variables.lastname : "Apellido"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+          <h3>${variables.province ? variables.province : "Province"}, ${
+    variables.country ? variables.country : "Country"
+  }</h3>
+          <ul>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -54,9 +58,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://media.licdn.com/dms/image/C4E16AQGFwsE_MQsMLA/profile-displaybackgroundimage-shrink_200_800/0/1658351366955?e=2147483647&v=beta&t=vh5DkGzwSPHVV4WRXcgg_mLAix7bb-Y3PoqyNZrRUE8",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/C4D03AQGAE-HXdKCg4Q/profile-displayphoto-shrink_800_800/0/1629458121970?e=2147483647&v=beta&t=Q0D2TSXkupGyHn5LBI4iCjPfzG9gMS-3xXmWK1PVzGY",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
