5df431146626f6ba033deda7201947f5e98b200d
Yúliya Bundur <ybundur@gmail.com>
2023-09-27 21:07:56 +0000

    changes applied

--- src/js/app.js
+++ src/js/app.js
@@ -40,12 +40,23 @@ function render(variables = {}) {
           <h3>${variables.province ? variables.province : "Province"}, ${
     variables.country ? variables.country : "Country"
   }</h3>
-          <ul>
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+
+  <ul class="${variables.socialMediaPosition}">
+            <li><a href=${
+              variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
+
+  
         </div>
     `;
 }
